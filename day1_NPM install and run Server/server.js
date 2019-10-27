d2bd54b2d00141c5185e847a35d8c1e69531d7de
eckysaroyd <enyato98@gmail.com>
2019-10-27 23:45:09 +0530

    connected to database

--- day1_NPM install and run Server/server.js
+++ day1_NPM install and run Server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send(`hellow people`));
+app.get("/", (req, res) => res.send(`hellow world`));
 
 const port = process.env.PORT || 3000;
 
--- day2/server.js
+++ day2/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("hellow Jacque one i like you"));
+//db config
+const db = require(`./config/keys`).mongoURI;
+
+//DB CONNECTION
+mongoose
+  .connect(db)
+  .then(() => console.log(`DB connected`))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send(`hellow World I'am Here`));
 
 const port = process.env.PORT || 2000;
 
-app.listen(port, () => console.log("server run on port ${port}"));
+app.listen(port, () => console.log(`server run on port ${port}`));
